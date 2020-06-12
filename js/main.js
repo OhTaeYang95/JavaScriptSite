3bc5e6081fbce1883869f32397c0e0081c0e1af0
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-06-12 22:51:00 +0900

    JS : Navbar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//  Make Navbar Transparent when it is on the Top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
